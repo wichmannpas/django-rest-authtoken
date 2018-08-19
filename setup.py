bb411e4e64b87102ef4bfdded6ca1a5b660c152f
Pascal Wichmann <git@pa-w.de>
2018-08-19 16:14:32 +0200

    Package for PyPI

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='django-rest-authtoken',
+    version='0.9',
+    author='Pascal Wichmann',
+    author_email='wichmannpas@gmail.com',
+    description='A simple auth token auth backend for Django Rest Framework storing cryptographically hashed tokens on server-side.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/wichmannpas/django-rest-authtoken',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+)

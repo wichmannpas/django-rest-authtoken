f44ba5d37576182c7b5feb870d6270b5fda95e5e
Pascal Wichmann <git@pa-w.de>
2020-08-22 16:12:33 +0200

    Bump version to 2.0.0: Backward-incompatible changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as fh:
 
 setuptools.setup(
     name='django-rest-authtoken',
-    version='1.2.4',
+    version='2.0.0',
     author='Pascal Wichmann',
     author_email='wichmannpas@gmail.com',
     description='A simple token-based auth backend for Django Rest Framework storing cryptographically hashed tokens on server-side.',

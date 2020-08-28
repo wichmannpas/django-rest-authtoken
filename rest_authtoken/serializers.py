d508b9490675ba139c4437232680bb2c29e20706
Pascal Wichmann <git@pa-w.de>
2020-08-28 20:58:48 +0200

    Registration: Validate username uniqueness by natural_key to allow for case-insensitive usernames

--- rest_authtoken/serializers.py
+++ rest_authtoken/serializers.py
@@ -26,7 +26,23 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
                 },
             }
 
-    def validate_password(self, value):
+    @staticmethod
+    def validate_username(value):
+        """
+        Make sure that no user with this natural key exists.
+        This can be used to make usernames case-insensitive, e.g.,
+        disallow the registration of two different usernames which
+        are equal but with different case.
+        """
+        try:
+            user = get_user_model().objects.get_by_natural_key(value)
+        except get_user_model().DoesNotExist:
+            return value
+        else:
+            raise ValidationError('already exists')
+
+    @staticmethod
+    def validate_password(value):
         if len(value) < 6:
             raise ValidationError('password too short')
         return value
